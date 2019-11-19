4b948d81bcdf6691bd0afa655688f7c70639a9f0
duvaltex-rami <rami.khouri@duvaltex.com>
2019-11-19 12:11:45 -0500

    commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
-    tags: ["Product one", "Product two", "Product three", "Product more"]
+    tags: ["Product one", "Product two", "Product three", "Product more "]
   };
 
   styles = {
